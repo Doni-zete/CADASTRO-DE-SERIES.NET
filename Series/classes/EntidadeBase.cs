cb7bb7f5d131726244e96f6889ca0993a9424ff1
Doni-zete <donizete112@gmail.com>
2021-08-21 23:15:41 -0300

    adicionando entidades

---
+++ Series/classes/EntidadeBase.cs
@@ -0,0 +1,12 @@
+namespace Series.classes
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+
+        {
+            EntidadeBase minhaClass = new EntidadeBase();
+            System.Console.WriteLine();
+        }
+    }
+}
